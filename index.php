c8e8e3d00906aacd438efb1f01730d50203e8a63
syckess <60026555+syckess@users.noreply.github.com>
2020-07-21 12:18:54 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
 
-echo "se subio la wea";
+$data = '{"Concepto":"Curso de PHP", "subtotal":"200", "ID":"1"}';
+$url = "https://codybotapi.firebaseio.com/presupuestos.json";
 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: text/plain'));
+
+$response = curl_exec($ch);
+if( curl_errno($ch)){
+    echo 'Error:'.curl_errno($ch);
+}else{
+    echo "Ya inserto";
+}
 
 ?>
